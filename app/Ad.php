fb32c2cd7e4f153427a756917fa5205e6c18bc11
Evandro Murilo <evandromurilob@hotmail.com>
2017-09-14 00:09:41 -0400

    funções auxiliares no Ad

--- app/Ad.php
+++ app/Ad.php
@@ -3,11 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Ad extends Model
 {
 	public function getCreatorAttribute()
 	{
 		return User::find($this->user_id);
+	}
+
+	public function getImgUrlAttribute()
+	{
+			if (Storage::exists('public/ads/' . $this->id)) {
+					return asset(Storage::url('public/ads/' . $this->id));
+			}
+			else {
+					return "https://i.pinimg.com/236x/18/63/cc/1863cc2425ad5f71b6b6bfddd64bb586--garfield.jpg";
+			}
+	}
+
+	public function getPriceFAttribute()
+	{
+			return 'R$ ' . number_format($this->price/100.0, 2, '.', ',');
 	}
 }
