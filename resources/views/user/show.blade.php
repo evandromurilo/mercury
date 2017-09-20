6d2f8aa9fbe84c992fd79ce8ca34c52823a1ff9f
valdeircesconeto <valdeircesconeto@gmail.com>
2017-09-19 23:11:49 -0400

    bugs para smartphone e tablet resolvido

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 	<h1> {{ $user->name }} </h1>
-	<div class="gravata-avatar">
-		@if (Storage::exists('public/users/' . $user->id))
-			<img id="avatar" src="{{ asset(Storage::url('public/users/' . $user->id)) }}">
-		@else
-			<img id="avatar" src="{{ $user->gravatar }}?s=120">
-		@endif
-	</div>
+		<div class="gravata-avatar">
+				@if (Storage::exists('public/users/' . $user->id))
+					<img id="avatar" src="{{ asset(Storage::url('public/users/' . $user->id)) }}">
+				@else
+					<img id="avatar" src="{{ $user->gravatar }}?s=120">
+				@endif
+		</div>
 	@if (Auth::check() && Auth::id() == $user->id)
 		<a class="alterar-a" href="{{ route('users.edit', Auth::id()) }}">Editar Perfil</a>
 	@endif
