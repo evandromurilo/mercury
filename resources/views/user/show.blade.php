2dddd8dde204bf836e48c6f9f7492a0aa3c7dff4
valdeircesconeto <valdeircesconeto@gmail.com>
2017-09-29 00:36:35 -0400

    user-show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -22,24 +22,42 @@
 </header>
 
 <article>
-	<div class="well">
-		<div class="row">
-			<h2>Anúncios</h2>
-			<ul id="rows-ul">
-					@foreach ($user->ads as $ad)
-						@if ($ad->price == 0)
-							<li id="rows-li"><i class="fa fa-gift" aria-hidden="true" style="color:green;"></i><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></li>
-						@else
-							<li id="rows-li"><i class="fa fa-usd" aria-hidden="true" style="color:#FFD700"></i><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></li>
-						@endif
-					@endforeach
-			</ul>
-			@if (Auth::check())
-				<a class="btn_messages" href="{{ route('messages.create', ['user' => $user->id]) }}">Enviar Mensagem<i class="fa fa-envelope-o" aria-hidden="true"></i></a>
-			@endif
+	<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+				<div class="well">
+
+					<h2>Anúncios</h2>
+					<ul id="rows-ul">
+							@foreach ($user->ads as $ad)
+								@if ($ad->price == 0)
+									<li onmouseover="Mover();" onmouseout="Tirar();" id="rows-li"><i class="fa fa-gift" aria-hidden="true" style="color:green;"></i><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></li>
+								@else
+									<li id="rows-li"><i class="fa fa-usd" aria-hidden="true" style="color:#FFD700"></i><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></li>
+								@endif
+							@endforeach
+					</ul>
+					@if (Auth::check())
+						<a class="btn_messages" href="{{ route('messages.create', ['user' => $user->id]) }}">Enviar Mensagem<i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+					@endif
+				</div>
+				</div>
+				<!-- coluna onde as informações tem que aparecer-->
+				<div class="col-md-8">
+					<span id="titulo"></span>
+					<spa
+					<!-- Aqui as informações deve aparecer com titulo conteudo-->
+			</div>
 		</div>
 	</div>
 </article>
-
-
+<!-- mano fiz aqui mais vai precisar de um lop mais acho que a ideia e essa-->
+<script type="text/javascript">
+	function Mover() {
+		document.getElementById('titulo').innerHTML = "{{ $ad->title }}" ;
+	}
+	function Tirar(){
+		document.getElementById('titulo').innerHTML = null;
+	}
+</script>
 @endsection
